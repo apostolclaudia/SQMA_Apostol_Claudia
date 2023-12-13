03d206505b639cedb090a95585a10d1b1e46be0f
Claudia Apostol <82058139+apostolclaudia@users.noreply.github.com>
2023-12-13 23:31:44 +0200

    Add files via upload

---
+++ calc.test.js
@@ -0,0 +1,23 @@
+const { add, subtract, multiply, divide } = require("./calc");
+
+describe("Calculator", () => {
+	test("Adding two numbers", () => {
+		expect(add(7, 3)).toBe(10);
+		expect(add(150, 150)).toBe(300);
+	});
+
+	test("Subtracting two numbers", () => {
+		expect(subtract(15, 5)).toBe(10);
+		expect(subtract(250, 150)).toBe(100);
+	});
+
+	test("Multiplying two numbers", () => {
+		expect(multiply(5, 20)).toBe(100);
+		expect(multiply(100, 200)).toBe(20000);
+	});
+
+	test("Dividing two numbers", () => {
+		expect(() => divide(12, 0)).toThrow("Cannot divide by zero");
+		expect(divide(120, 12)).toBe(10);
+	});
+});
