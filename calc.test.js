138965ac600edee85a8c71d795d87a84210b2c0c
apostolclaudia <claudia.apostol00@gmail.com>
2023-12-14 18:21:02 +0200

    modify test cases

--- calc.test.js
+++ calc.test.js
@@ -1,34 +1,29 @@
 const { add, subtract, multiply, divide } = require("./calc");
+const testSuite = process.env.TEST_SUITE;
 
 describe("Addition Suite", () => {
-	const testSuite = process.env.TEST_SUITE;
-
 	if (testSuite === "AdditionSuite") {
 		test("Adding two numbers", () => {
 			expect(add(7, 3)).toBe(10);
 			expect(add(150, 150)).toBe(300);
 		});
 	} else {
-		test.skip("Addition Suite tests skipped");
+		test.todo("Adding two numbers");
 	}
 });
 
 describe("Subtraction Suite", () => {
-	const testSuite = process.env.TEST_SUITE;
-
 	if (testSuite === "SubtractionSuite") {
 		test("Subtracting two numbers", () => {
 			expect(subtract(15, 5)).toBe(10);
 			expect(subtract(250, 150)).toBe(100);
 		});
 	} else {
-		test.skip("Subtraction Suite tests skipped");
+		test.todo("Subtracting two numbers");
 	}
 });
 
 describe("Multiplication and Division Suite", () => {
-	const testSuite = process.env.TEST_SUITE;
-
 	if (testSuite === "MultiplyDivideSuite") {
 		test("Multiplying two numbers", () => {
 			expect(multiply(5, 20)).toBe(100);
@@ -40,6 +35,6 @@ describe("Multiplication and Division Suite", () => {
 			expect(divide(120, 12)).toBe(10);
 		});
 	} else {
-		test.skip("Multiplication and Division Suite tests skipped");
+		test.todo("Multiplication and Division Suite tests");
 	}
 });
